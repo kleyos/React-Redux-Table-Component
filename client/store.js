4b355b1425bd7265001021bc6414476636601763
Olga Erushenko <olga.erushenko@gmail.com>
2017-04-12 13:08:38 +0300

    add to store redux dev tools using compose

--- client/store.js
+++ client/store.js
@@ -1,15 +1,18 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers/index'
 import logger from 'redux-logger'
 import reduxThunk from 'redux-thunk'
 
-const store = createStore(rootReducer, applyMiddleware(logger, reduxThunk))
 
-// if (module.hot) {
-//     module.hot.accept('./reducers', () => {
-//       const nextRootReducer = require('./reducers')
-//       store.replaceReducer(nextRootReducer)
-//     })
-//   }
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer,  composeEnhancers(applyMiddleware(logger, reduxThunk)) )
+
+if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      const nextRootReducer = require('./reducers')
+      store.replaceReducer(nextRootReducer)
+    })
+  }
 
 export default store
